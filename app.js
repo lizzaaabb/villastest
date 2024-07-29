88ae58cb2a840da603c181118b7efc5a91a339e5
lizzaaabb <lbogveradze12@gmail.com>
2024-07-29 13:00:42 +0400

    slideshow container chavasworet

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const menu = document.getElementById("menu-toggle")
 const sidebar = document.getElementById("sidebar")
+const slideshow = document.getElementById("slideshow-container");
 
 menu.onclick = function(){
     sidebar.classList.toggle("show")
 
+    if (sidebar.classList.contains("show")) {
+        // Hide slideshow with animation
+        slideshow.classList.add("slide-out");
+        slideshow.classList.remove("slide-in");
+    } else {
+        // Show slideshow with animation
+        slideshow.classList.remove("slide-out");
+        slideshow.classList.add("slide-in");
+    }
+    
 }
 
 var slideIndex = 1;
@@ -30,3 +41,4 @@ function showSlides(n){
     slides[slideIndex - 1].style.display = "block"
     dots[slideIndex - 1].className += "active"
 }
+
